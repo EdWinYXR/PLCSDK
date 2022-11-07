06e70575bade170b14111a37994bad6a8c236e7e
yinxurong <a1410648838@163.com>
2022-11-07 17:27:54 +0800

    第一版，只有opc

--- OPCDemo/Program.cs
+++ OPCDemo/Program.cs
@@ -10,16 +10,23 @@ namespace OPCDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async void Main(string[] args)
         {
+            OPCClient.Instance.InIt("127.0.0.1", 49320, true);
+            #region write
             Dictionary<string, string> dic = new Dictionary<string, string>() {
                 { "ns=2;s=Channel9.Device1._System._Enabled","false" }
             };
-            string str = "MM-dd-HH ss:fff ";
-            Console.WriteLine(str.Length);
-
-            OPCClient.Instance.InIt("127.0.0.1", 49320, true);
-
+            OPCClient.Instance.WriteAsync(dic);
+            #endregion
+            #region Read
+            List<string> path = new List<string>() { "ns=2;s=Channel9.Device1._System._Enabled" };
+            var values=await OPCClient.Instance.ReadAsync(path);
+            foreach(var key in values.Keys)
+            {
+                Console.WriteLine(string.Format("path={0},value={1}", key, values[key]));
+            }
+            #endregion
             Thread.Sleep(100000);
             Console.WriteLine(OPCClient.Instance.LogTxt.ToString());
 
--- OPCManager/OPCClient.cs
+++ OPCManager/OPCClient.cs
@@ -2,13 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 /*
     个人微信：a7761075
